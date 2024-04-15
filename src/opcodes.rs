df69b93fc3eb502c5b9641a6614e81d884fa61df
mefreanzy <freanzyme@gmail.com>
2024-04-15 16:11:23 +0300

    add: added opcode structure and mod.

---
+++ src/opcodes.rs
@@ -0,0 +1,19 @@
+pub enum Opcode
+{
+    BR = 0,
+    ADD,
+    LD,
+    ST,
+    JSR,
+    AND,
+    LDR,
+    STR,
+    RTI,
+    NOT,
+    LDI,
+    STI,
+    JMP,
+    RES,
+    LEA,
+    TRAP,
+}
