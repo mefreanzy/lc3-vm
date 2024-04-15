35bbeeb2bbcd7de1485cadf192048db6f427e9a5
mefreanzy <freanzyme@gmail.com>
2024-04-15 15:27:14 +0300

    update: lc3 structure defined.

--- src/lc3.rs
+++ src/lc3.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use lc3_vm::register::Registers;
+
+const MEMORY_SIZE: usize = 1 << 16;
+pub struct VM 
+{
+    memory: [u16; MEMORY_SIZE],
+    reg: Registers,
 }
