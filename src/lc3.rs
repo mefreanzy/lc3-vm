2988a30cc6d066ce3e40252866ed9fc4be036be6
mefreanzy <freanzyme@gmail.com>
2024-04-27 14:16:35 +0300

    update: functions necessary for the program to work.

--- src/lc3.rs
+++ src/lc3.rs
@@ -1,8 +1,33 @@
 use crate::register::Registers;
+use crate::instructions::OpCode;
 
 const MEMORY_SIZE: usize = 1 << 16;
 pub struct VM 
 {
     pub memory: [u16; MEMORY_SIZE],
     pub reg: Registers,
+}
+
+impl VM 
+{
+    pub fn read_memory(&self, address: u16) -> u16
+    {
+        self.memory[address as usize]
+    }
+
+    pub fn write_memory(&mut self, address: u16, data: u16)
+    {
+        self.memory[address as usize] = data;
+    }
+
+    fn run(&mut self)
+    {
+        loop {
+            let pc = self.reg.pc;
+            let instruction = self.memory[pc as usize];
+            let opcode = OpCode::from_u16(instruction).expect("Invalid opcode.");
+            opcode.execute(self, instruction);
+            self.reg.pc += 1;
+        }
+    }
 }
