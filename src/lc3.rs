6ec45e015513bb3efb3649246ffb757d76761169
mefreanzy <freanzyme@gmail.com>
2024-04-28 23:58:59 +0300

    update: the main function for the virtual machine to function is complete.

--- src/lc3.rs
+++ src/lc3.rs
@@ -1,5 +1,9 @@
 use crate::register::Registers;
-use crate::instructions::OpCode;
+use crate::instructions::OpCode
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 const MEMORY_SIZE: usize = 1 << 16;
 pub struct VM 
@@ -10,6 +14,14 @@ pub struct VM
 
 impl VM 
 {
+    pub fn new() -> Self
+    {
+        let memory = [0; MEMORY_SIZE];
+        let reg = Registers::new(0);
+
+        VM { memory, reg }
+    }
+
     pub fn read_memory(&self, address: u16) -> u16
     {
         self.memory[address as usize]
@@ -30,4 +42,36 @@ impl VM
             self.reg.pc += 1;
         }
     }
+}
+
+
+fn main()
+{
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 
+    {
+        eprintln!("Usage cargo run <program-file.obj>");
+        std::process::exit(1);
+    }
+
+    let fname = &args[1];
+    let f = File::open(fname).expect("Failed to open file");
+    let reader = io::BufReader::new(file);
+
+    let mut vm = VM::new();
+
+
+    for line in reader.lines()
+    {
+        if let Ok(line) = line
+        {
+            let opcode = OpCode::from_u16(instruction).expect("Invalid opcode.");
+            opcode.execute(&mut vm, instruction);
+        } else {
+            eprintln!("Invalid instruction: {}", line)
+        }
+    }
+
+    vm.run();
 }
